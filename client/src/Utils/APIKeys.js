4b5171de4ff7ba23026f0316467e8443b8104374
unknown <mrashes2@gmail.com>
2017-11-18 15:17:19 -0600

    removed old file

--- client/src/Utils/APIKeys.js
+++
@@ -1,7 +0,0 @@
-alterMapsSrc = function () {
-    const initalNode = document.getElementById('googleMapsNode');
-    const key = process.env.mapsKey || "AIzaSyC8bLOv4xelVAZYHzBrbV_2Haw_TlT_5Ag";
-    let src = "https://maps.googleapis.com/maps/api/js?key="+key+"&libraries=places"
-    console.log(src)
-}
-alterMapsSrc()
